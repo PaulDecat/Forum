55a436d504f03c434b980724efcb3c345f3fce29
CryptoDebug <axel10.lemaitre@gmail.com>
2024-05-31 10:39:44 +0200

    Récupératio des posts sur l'index

--- main.go
+++ main.go
@@ -9,23 +9,24 @@ import (
 
 func main() {
 	var err error
-	db, err = initDB("./blog.db")
+	db, err = initDB("./data.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	fileServer := http.FileServer(http.Dir("./templates"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/index", indexHandler)
-	http.HandleFunc("/createP", createPostHandler)
-	http.HandleFunc("/mypage", mypageHandler)
-	http.HandleFunc("/parameters", parametersHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/signup", signUpHandler)
+	http.HandleFunc("/createP", createPostHandler)
+	http.HandleFunc("/submituser", submitUserHandler)
+	http.HandleFunc("/submitpost", submitPostHandler)
 	http.HandleFunc("/terms", termsHandler)
 	http.HandleFunc("/rgpd", rgpdHandler)
-	http.HandleFunc("/submituser", submitUserHandler)
+	http.HandleFunc("/mypage", mypageHandler)
+	http.HandleFunc("/parameters", parametersHandler)
 
-	log.Println("Starting server at http://localhost:8080\n")
+	log.Println("Starting server at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- templates/script.js
+++ templates/script.js
@@ -20,5 +20,3 @@ fetch('createp.html')
     };
     tp.send();
   });
-
-  
